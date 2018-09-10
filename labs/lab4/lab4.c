28c248ee92da14797bbb38ab3a12697cd882d169
David Ramirez <davidsrnfire@gmail.com>
2018-09-09 22:10:37 -0500

    Fixing lab 4

--- labs/lab4/lab4.c
+++ labs/lab4/lab4.c
@@ -23,89 +23,104 @@
 #include <string.h>
 
 #define SIZE  1000
-#define WIDTH   68
 
 float nums[SIZE];
 
-int main(int argc, char *argv[]) {
-  if(argc != 3) {
-    printf("Usage: %s [input_directory] [output_directory]\n", argv[0]);
-    return -1;
-  }
-  DIR* dir;
-  DIR* dir2;
-  struct dirent* direntry;
-  char filename[PATH_MAX + NAME_MAX + 1];
-  char out_filename[PATH_MAX + NAME_MAX + 1];
-  FILE *fd_in;
-  FILE *fd_out;
-  int time, inter, size, i;
-  float num;
-  int min, max;
-  min=INT_MAX;
-  max=INT_MIN;
-
-
-  if((dir = opendir(argv[1])) == NULL || (dir2 = opendir(argv[2])) == NULL ) {
-    perror(argv[0]);
-    return -1;
-  }
-
-  fd_out = fopen(argv[2], "w");
-
-  while ((direntry = readdir(dir)) != NULL) {
-    if(strcmp(direntry->d_name, ".") != 0 && strcmp(direntry->d_name, "..") != 0) {
-      if(direntry->d_type != DT_DIR) {
-        // printf("%s\n", direntry->d_name);
-        sprintf(filename, "%s/%s", argv[1], direntry->d_name);
-        sprintf(out_filename, "%s/%s", argv[2], direntry->d_name);
-        // printf("%s\n", out_filename);
-        fd_in = fopen(filename, "r");
-        // char outputfile = strcat(argv[2], filename);
-        if (fd_in == NULL) {
-          perror(argv[0]);
-          return -1;
+int main(int argc, char *argv[]){
+        if(argc !=3){
+                printf("Usage: %s [input_directory] [output_directory]\n", argv[0]);
+                return -1;
         }
-        memset(nums, 0, SIZE * sizeof(float));
-        while(fscanf(fd_in, "%i,%i", &time, &inter) != EOF){
-          size = inter * inter;
-          for (i = 0; i < size; i++) {
-            fscanf(fd_in, ",%f", &num);
-            nums[i] = (float)num;
-            if(nums[i] < min) {
-              min = nums[i];
-            }
-            if(nums[i]> max) {
-              max = nums[i];
-            }
-          }
-        }
-        fclose(fd_in);
-        memset(nums, 0, SIZE * sizeof(float));
-        fd_in = fopen(filename, "r");
-        fd_out= fopen(out_filename, "w");
-        // printf("Reading file %s\n", filename);
-        // printf("Copying file %s to %s\n", filename, out_filename);
-        // printf("MAX:%i MIN:%i\n", max, min);
-        // fd_out = fopen(outputfile, "w");
-        while ( fscanf(fd_in, "%i,%i", &time, &inter) != EOF ) {
-          size = inter * inter;
-          for (i = 0; i < size; i++) {
-            // printf("%.2f\t", num);
-            fscanf(fd_in, ",%f", &num);
-            nums[i] = (float)num;
-            nums[i] = (nums[i]-min)/(max-min);
-            fprintf(fd_out, "%f,", nums[i]);
-            // printf("%f", nums[i]);
-          }
-          fprintf(fd_out, "\n");
-
-        }
-        fclose(fd_in);
-        fclose(fd_out);
+        DIR* dir;
+        DIR* dir2;
+        struct dirent* direntry;
+        char filename[PATH_MAX + NAME_MAX + 1];
+        char out_filename[PATH_MAX + NAME_MAX + 1];
+        FILE *fd_in;
+        FILE *fd_out;
+        int time, inter, size, i, line=0;
+        float num;
+        float min, max;
         min=INT_MAX;
         max=INT_MIN;
-      }
-    }
-  }
+        float minarg[SIZE], maxarg[SIZE];
+
+
+        if((dir=opendir(argv[1]))==NULL || (dir2=opendir(argv[2]))==NULL){
+                perror(argv[0]);
+                return -1;
+        }
+
+        fd_out=fopen(argv[2], "w");
+
+        while ((direntry=readdir(dir)) !=NULL){
+                if(strcmp(direntry->d_name, ".") !=0 && strcmp(direntry->d_name, "..") !=0){
+                        if(direntry->d_type !=DT_DIR){
+                                // printf("%s\n", direntry->d_name);
+                                sprintf(filename, "%s/%s", argv[1], direntry->d_name);
+                                sprintf(out_filename, "%s/%s", argv[2], direntry->d_name);
+                                // printf("%s\n", out_filename);
+                                fd_in=fopen(filename, "r");
+                                // char outputfile=strcat(argv[2], filename);
+                                if (fd_in==NULL){
+                                        perror(argv[0]);
+                                        return -1;
+                                }
+                                // printf("%s\n", filename);
+                                memset(nums, 0, SIZE * sizeof(float));
+                                line=0;
+                                while(fscanf(fd_in, "%i,%i", &time, &inter) !=EOF){
+                                        // while(fscanf(fd_in, "%i,%i", &time, &inter) !=EOF){
+                                        line++;
+                                        min=INT_MAX;
+                                        max=INT_MIN;
+                                        // printf("%i\n", line);
+                                        size=inter * inter;
+                                        for (i=0; i < 626; i++){
+                                                fscanf(fd_in, ",%f", &num);
+                                                nums[i]=(float)num;
+                                                if(nums[i] < min){
+                                                        min=nums[i];
+                                                }
+                                                if(nums[i]> max){
+                                                        max=nums[i];
+                                                }
+                                                minarg[line]=min;
+                                                maxarg[line]=max;
+                                        }
+                                        // printf("%s\n","working");
+                                }
+                                fclose(fd_in);
+                                line=0;
+                                memset(nums, 0, SIZE * sizeof(float));
+                                fd_in=fopen(filename, "r");
+                                fd_out=fopen(out_filename, "w");
+                                // printf("Reading file %s\n", filename);
+                                // printf("Copying file %s to %s\n", filename, out_filename);
+                                // fd_out=fopen(outputfile, "w");
+                                while (fscanf(fd_in, "%i,%i", &time, &inter) !=EOF){
+                                        fprintf(fd_out, "%i,%i", time, inter);
+                                        size=inter * inter;
+                                        // printf("%s\n", "working");
+                                        line++;
+                                        for (i=0; i < 626; i++){
+                                                // printf("MAX:%f MIN:%f\n", maxarg[line], minarg[line]);
+                                                // printf("%.2f\t", num);
+                                                fscanf(fd_in, ",%f", &num);
+                                                nums[i]=(float)num;
+                                                nums[i]=(nums[i]-minarg[line])/(maxarg[line]-minarg[line]);
+                                                // printf("%f\n", nums[i]);
+                                                fprintf(fd_out, ",%f", nums[i]);
+                                                // printf("%f", nums[i]);
+                                        }
+                                        fprintf(fd_out, "\n");
+
+                                }
+                                fclose(fd_in);
+                                fclose(fd_out);
+                                min=INT_MAX;
+                                max=INT_MIN;
+                        }
+                }
+        }
 }
