20e627f3d9290ae6bd3efb15179da40e1284116d
David Ramirez <davidsrnfire@gmail.com>
2018-09-23 23:33:12 -0500

    Correción de Proyecto

--- proyecto1/proyecto.c
+++ proyecto1/proyecto.c
@@ -15,7 +15,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
-#include <time.h>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <limits.h>
@@ -28,10 +27,12 @@
 float nums[SIZE];
 
 int main(int argc, char *argv[]){
+
   if(argc !=5){
     printf("Usage: %s [working_directory] [log_filename] [number_of_files] [timeout]\n", argv[0]);
     return -1;
   }
+
   DIR* dir;
 
   if((dir=opendir(argv[1]))==NULL){
@@ -42,37 +43,45 @@ int main(int argc, char *argv[]){
   char filename[PATH_MAX + NAME_MAX + 1];
   char newfilename[PATH_MAX + NAME_MAX + 1];
   char *extention="log", *name=argv[2];
-  int times = atoi(argv[3]), time=atoi(argv[4]), i, j, k, lim;
+  char str1[10], str2[10];
+
+  int times = atoi(argv[3]), time=atoi(argv[4]), num;
+
+  if(times <= 0 || time <= 0){
+    printf("%s\n", "number_of_files and timeout should be greater than 0");
+    return -1;
+  }
+
   sprintf(filename, "%s/%s.%s", argv[1], name, extention);
   FILE *fd;
   fd=fopen(filename, "r");
+
   if(fd==NULL){
     printf("%s\n", "Log file does not exist");
     return -1;
   }
+
   fclose(fd);
-  for(i = 0; i<times; i++){
-    for(lim = i; lim > 0; lim--){
-      int num = lim;
-      char str3[10];
-      char str4[10];
-      sprintf(str3, "%d", num);
-      sprintf(str4, "%d", num+1);
-      sprintf(filename, "%s/%s.%s", argv[1], name, str3);
-      sprintf(newfilename, "%s/%s.%s", argv[1], name, str4);
+  for(int i = 0; i<=times; i++){
+    for(int lim = i; lim > 0; lim--){
+      num = lim;
+      sprintf(str1, "%d", num);
+      sprintf(str2, "%d", num+1);
+      sprintf(filename, "%s/%s.%s", argv[1], name, str1);
+      sprintf(newfilename, "%s/%s.%s", argv[1], name, str2);
       rename(filename, newfilename);
     }
-    j=i;
-    k=j+1;
-    char str1[10];
-    char str2[10];
-    sprintf(str1, "%d", j);
-    sprintf(str2, "%d", k);
     sprintf(filename, "%s/%s.%s", argv[1], name, extention);
     sprintf(newfilename, "%s/%s.%s", argv[1], name, "1");
     rename(filename, newfilename);
     fd = fopen(filename, "w");
     fclose(fd);
+
+    if(i == times){
+      sprintf(str1, "%d", times+1);
+      sprintf(filename, "%s/%s.%s", argv[1], name, str1);
+      remove(filename);
+    }
 
     sleep(time);
   }
